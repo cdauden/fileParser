5d6fae281ee929d4e144290eba49289903185d71
cdauden <chrisdauden@gmail.com>
2023-06-21 12:45:20 -0400

    update2

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-import pandas as pd
+#import pandas as pd
+import csv
 
+readPath=r""
+writePath=r""
+
+f=open(writePath,"w")
+
+# Opens and closes file with encoding cp='437' to decode all characters in file
+with open(readPath, mode='r', encoding='cp437')as file:
+    csvReader=csv.reader(file, delimiter=",")
+    for row in csvReader:
+        # row[6] is the User Group column
+        if row[6]=='Students':
+            # row[0] is the Email column
+            f.write(row[0]+'\n')
+            print(row[0])
+f.close()
+
+'''
 readPath=r""
 writePath=r""
 
@@ -19,3 +37,4 @@ while rows<len(array):
     rows+=1
 
 f.close()
+'''
