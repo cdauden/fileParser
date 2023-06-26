3962864041aa8d215feae427325426083ec30f69
cdauden <chrisdauden@gmail.com>
2023-06-26 09:20:30 -0400

    update3

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-import pandas as pd
+import csv
+import argparse
 
-readPath=r""
-writePath=r""
+ap=argparse.ArgumentParser()
 
-# saves Email and User Group columns to dataframe
-df=pd.read_csv(readPath, usecols=['Email','User Group'])
+ap.add_argument('-r', help='file path of the reading file')
+ap.add_argument('-w', help='file path of file that is being written or created')
 
-# dataframe to array
-array=df[['Email','User Group']].to_numpy()
+args, unknown=ap.parse_known_args()
 
-rows=0
-f=open(writePath,"w")
+# Opens and closes file with encoding 'utf-8' to decode all characters in file
 
-while rows<len(array):
-    if(array[rows,1]=='Students'):
-        print(array[rows,0])
-        #f.write(array[rows,0]+"\n")
-    rows+=1
-
-f.close()
+if args.r is not None and args.w is not None:
+    f = open(args.w, "w")
+    with open(args.r, mode='r', encoding='utf-8')as file:
+        csvReader=csv.reader(file, delimiter=",")
+        for row in csvReader:
+            # row[6] is the User Group column
+            if row[6]=='Students':
+                # row[0] is the Email column
+                f.write(row[0]+'\n')
+                print(row[0])
+    f.close()
+else:
+    ap.print_help()
